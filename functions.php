6c0723839bbbcebafde21cdcaadef37ab3e1ee20
HyKo <punk.flag8609@fastmail.com>
2024-08-15 13:48:14 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,19 +5,14 @@
 	});
 
 	add_action( 'customize_register', function($wp_customize) {
-		// Ajouter la section pour la couleur principale
 		$wp_customize->add_section('qlic_acary_colors', array(
 			'title'    => __('Couleurs du thème', 'qlic-acary'),
 			'priority' => 30,
 		));
-
-		// Ajouter le paramètre pour la couleur principale
 		$wp_customize->add_setting('qlic_acary_main_color', array(
 			'default'   => '#0073aa',
 			'transport' => 'refresh',
 		));
-
-		// Ajouter le contrôle pour choisir la couleur
 		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'qlic_acary_main_color', array(
 			'label'    => __('Couleur principale', 'qlic-acary'),
 			'section'  => 'qlic_acary_colors',
@@ -25,27 +20,24 @@
 		)));
 	});
 
-	// Générer le CSS en ligne pour appliquer la couleur principale
 	function qlic_acary_custom_colors() {
 		$main_color = get_theme_mod('qlic_acary_main_color', '#0073aa');
-
 		$custom_css = "
-			/* Changer la couleur de fond lors du surlignage */
 			::selection {
 				background-color: $main_color;
-				color: #ffffff; /* Couleur du texte surligné */
+				color: #ffffff;
 			}
-
-			/* Changer la couleur par défaut des liens */
+			::-moz-selection {
+				background-color: $main_color;
+				color: #ffffff;
+			}
 			a {
 				color: $main_color;
 			}
-
 			a:hover, a:focus {
 				color: darken($main_color, 10%);
 			}
 		";
-
 		return $custom_css;
 	}
 
