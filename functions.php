61d786e5358092f40ae1739f3dd4726cd5b2db43
HyKo <punk.flag8609@fastmail.com>
2025-01-04 14:00:14 -0500

    1.0.2

--- functions.php
+++ functions.php
@@ -1,46 +1,46 @@
 <?php
 	add_action( 'wp_enqueue_scripts', function() {
-		wp_enqueue_style( 'qlic-acary-theme-css', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all' );
-		wp_add_inline_style( 'qlic-acary-theme-css', qlic_acary_custom_colors() );
+		wp_enqueue_style( 'qlic-hyko-theme-css', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all' );
+		wp_add_inline_style( 'qlic-hyko-theme-css', qlic_hyko_custom_colors() );
 	});
 
 	add_action( 'admin_enqueue_scripts', function() {
-		wp_enqueue_style( 'qlic-acary-admin-css', get_template_directory_uri() . '/admin.css', array(), '1.0.0', 'all' );
+		wp_enqueue_style( 'qlic-hyko-admin-css', get_template_directory_uri() . '/admin.css', array(), '1.0.0', 'all' );
 	});
 
 	add_action( 'customize_register', function($wp_customize) {
 		// Section existante pour les couleurs
-		$wp_customize->add_section('qlic_acary_colors', array(
-			'title'    => __('Couleurs du thème', 'qlic-acary'),
+		$wp_customize->add_section('qlic_hyko_colors', array(
+			'title'    => __('Couleurs du thème', 'qlic-hyko'),
 			'priority' => 30,
 		));
 
 		// Option pour la couleur principale
-		$wp_customize->add_setting('qlic_acary_main_color', array(
+		$wp_customize->add_setting('qlic_hyko_main_color', array(
 			'default'   => '#0089C9FF',
 			'transport' => 'refresh',
 		));
-		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'qlic_acary_main_color', array(
-			'label'    => __('Couleur principale', 'qlic-acary'),
-			'section'  => 'qlic_acary_colors',
-			'settings' => 'qlic_acary_main_color',
+		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'qlic_hyko_main_color', array(
+			'label'    => __('Couleur principale', 'qlic-hyko'),
+			'section'  => 'qlic_hyko_colors',
+			'settings' => 'qlic_hyko_main_color',
 		)));
 
 		// Option pour la couleur secondaire
-		$wp_customize->add_setting('qlic_acary_secondary_color', array(
+		$wp_customize->add_setting('qlic_hyko_secondary_color', array(
 			'default'   => '#057727FF',
 			'transport' => 'refresh',
 		));
-		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'qlic_acary_secondary_color', array(
-			'label'    => __('Couleur secondaire', 'qlic-acary'),
-			'section'  => 'qlic_acary_colors',
-			'settings' => 'qlic_acary_secondary_color',
+		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'qlic_hyko_secondary_color', array(
+			'label'    => __('Couleur secondaire', 'qlic-hyko'),
+			'section'  => 'qlic_hyko_colors',
+			'settings' => 'qlic_hyko_secondary_color',
 		)));
 	});
 
-	function qlic_acary_custom_colors() {
-		$main_color = get_theme_mod('qlic_acary_main_color', '#0089C9FF');
-		$secondary_color = get_theme_mod('qlic_acary_secondary_color', '#057727FF'); // Récupère la couleur secondaire
+	function qlic_hyko_custom_colors() {
+		$main_color = get_theme_mod('qlic_hyko_main_color', '#0089C9FF');
+		$secondary_color = get_theme_mod('qlic_hyko_secondary_color', '#057727FF'); // Récupère la couleur secondaire
 		$custom_css = "
 			::selection {
 				background-color: $main_color;
